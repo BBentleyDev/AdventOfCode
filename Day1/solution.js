ad1796643997a3663ee2e8d6be152afad3241bfd
Brandon Bentley <bbentleydev@gmail.com>
2022-12-06 17:48:49 -0500

    Completed day 1 solution

--- Day1/solution.js
+++ Day1/solution.js
@@ -3,17 +3,27 @@ const { readFileSync } = require('fs');
 
 // Get input data of groups of numbers separated by blank lines.
 const input = readFileSync(
-    path.join(__dirname, "./input.txt")
-  ).toString();
+  path.join(__dirname, "./input.txt")
+).toString().trim().split('\n\n');
 
-let caloriesPerElf = input.split('\n\n').map(group => { 
-    return group
-        .split('\n')
-        .map(group => Number(group))
-        .reduce( (total, calories) => total + calories)
+//calculate the total calories carried by each elf
+let caloriesPerElf = input.map(group => {
+  return group
+    .split('\n')
+    .map(item => Number(item))
+    .reduce((total, calories) => total + calories, 0)
 })
+  .sort((a, b) => b - a)
 
-const most = Math.max(...caloriesPerElf)
+//Find the elf carrying the most calories
+const most = caloriesPerElf[0]
 
+//Print answer
 console.log(most)
+
+
+//PART 2
+
+//Print the total calories of first three elves
+console.log(caloriesPerElf[0] + caloriesPerElf[1] + caloriesPerElf[2])
 
